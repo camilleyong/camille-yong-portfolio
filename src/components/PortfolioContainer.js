3dc483af672599f0b6d2741626676d6a5207f1c6
Camille Yong <camillionaire@Camilles-MacBook-Pro.local>
2022-10-27 10:42:45 -0700

    fixed deployment error

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -4,6 +4,7 @@ import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
+import './css/portfolioContainer.css'
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -1,83 +1,22 @@
-import React, { useState } from 'react';
-// import './style.css';
+import React from 'react';
 
 export default function Contact() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-  const [status, setStatus] = useState('');
-
-  const handleInputChange = (e) => {
-    // Based on the input type, we set the state of either email, username, and password
-    if (inputType === 'email') {
-      setEmail(inputValue);
-    } else if (inputType === 'name') {
-      setName(inputValue);
-    } else {
-      setMessage(inputValue);
-    }
-  };
-
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
-
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !userName) {
-      setErrorMessage('Email or username is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-    }
-    if (!checkPassword(password)) {
-      setErrorMessage(
-        `Choose a more secure password for the account: ${userName}`
-      );
-      return;
-    }
-    alert(`Hello ${userName}`);
-
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setUserName('');
-    setPassword('');
-    setEmail('');
-  };
-
   return (
     <div>
-      <h1>Contact Me</h1>
-      <p>Hello {userName}</p>
-      <form className="form">
-        <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="email"
-          placeholder="email"
-        />
-        <input
-          value={userName}
-          name="userName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="username"
-        />
-        <input
-          value={password}
-          name="password"
-          onChange={handleInputChange}
-          type="password"
-          placeholder="Password"
-        />
-        <button type="button" onClick={handleFormSubmit}>Submit</button>
-      </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
+      <h1>Contact Page</h1>
+      <p>
+        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
+        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
+        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
+        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
+        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
+        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
+        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
+        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
+        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
+        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
+        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+      </p>
     </div>
   );
 }
